1967ed22d442a9d53e2027aaab365b78428438c9
Marcelo Bittar <marcelo.usjt@hotmail.com>
2021-06-17 11:28:22 -0300

    Add project files.

---
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SmartSchool.API.Models;
+using SmartSchool.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        //Para chamar a classe do context
+        private readonly SmartContext _context;
+
+        //Utiliza a classe aluno do contexto
+        public AlunoController(SmartContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<AlunoController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Alunos);
+        }
+
+        // Get: api/aluno/byId?id={id} = Antigo
+        // Get: api/aluno/byId/{id} = atual
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) 
+                return BadRequest("O Aluno não foi encontrado");
+            
+            
+            return Ok(aluno);
+        }
+
+        // Get: api/aluno/{nome} = Antigo
+        //Get: api/aluno/ByName?nome={nome}&sobrenome={sobrenome}
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            //var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome));
+            //if (aluno == null)
+            //    return BadRequest("O Aluno não foi encontrado");
+
+            var aluno = _context.Alunos.FirstOrDefault(a =>
+                //a.Nome.Contains(nome, StringComparison.OrdinalIgnoreCase) && a.Sobrenome.Contains(sobrenome, StringComparison.OrdinalIgnoreCase)
+                a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome)
+            );
+
+            return Ok(aluno);
+        }
+
+        // api/aluno
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            _context.Add(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
+
+        }
+
+        // utilizado para atualizar um registro
+        // api/aluno
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (alu == null)
+            {
+                return BadRequest("Aluno não encontrado");
+            }
+
+            _context.Update(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
+
+        }
+
+        // utilizado para atualizar um registro parcialmente
+        // api/aluno
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (alu == null)
+            {
+                return BadRequest("Aluno não encontrado");
+            }
+
+            _context.Update(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
+
+        }
+
+        // utilizado para deletar um registro
+        // api/aluno
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null)
+            {
+                return BadRequest("Aluno não encontrado");
+            }
+
+            _context.Remove(aluno);
+            _context.SaveChanges();
+            return Ok();
+
+        }
+
+
+    }
+}
+
---
+++ SmartSchool.API/Controllers/ProfessorController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.API.Data;
+using SmartSchool.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorController : ControllerBase
+    {
+        private readonly SmartContext _context;
+
+        public ProfessorController(SmartContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Professores);
+        }
+
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var professor = _context.Professores.FirstOrDefault(a => a.Id == id);
+            if (professor == null)
+                return BadRequest("O Professor não foi encontrado");
+
+
+            return Ok(professor);
+        }
+
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome)
+        {
+            var professor = _context.Professores.FirstOrDefault(a =>
+                a.Nome.Contains(nome)
+            );
+
+            return Ok(professor);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Professor professor)
+        {
+            _context.Add(professor);
+            _context.SaveChanges();
+            return Ok(professor);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Professor professor)
+        {
+            var prof = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (prof == null)
+            {
+                return BadRequest("Professor não encontrado");
+            }
+
+            _context.Update(professor);
+            _context.SaveChanges();
+            return Ok(professor);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Professor professor)
+        {
+            var prof = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (prof == null)
+            {
+                return BadRequest("Professor não encontrado");
+            }
+
+            _context.Update(professor);
+            _context.SaveChanges();
+            return Ok(professor);
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var professor = _context.Alunos.FirstOrDefault(a => a.Id == id);
+            if (professor == null)
+            {
+                return BadRequest("Professor não encontrado");
+            }
+
+            _context.Remove(professor);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
